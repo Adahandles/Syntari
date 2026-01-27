e28e1c06678559170920af1b4d73a6b08646673c
HOSKDOG <161114704+Adahandles@users.noreply.github.com>
2026-01-26 22:06:29 -0500

    Update bytecode.py with new content

--- src/compiler/bytecode.py
+++ src/compiler/bytecode.py
@@ -384,7 +384,7 @@ class BytecodeGenerator:
             else:
                 # Fallback for unknown types
                 bs = f"S{str(c)}".encode("utf-8")
-            
+
             data.extend(struct.pack("<I", len(bs)))
             data.extend(bs)
 
